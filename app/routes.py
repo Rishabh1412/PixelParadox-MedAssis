0f69aa97f4d275ed24068587ec99e4563b3b481a
Digvijay2245 <digvijaytomarprof@gmail.com>
2024-08-24 15:37:12 +0530

    mail formatting

--- app/routes.py
+++ app/routes.py
@@ -128,14 +128,14 @@ def logout():
 
 @app.route('/diabetes-form', methods=['GET','POST'])
 def diabetes():
-    diabetesform=DiabetesForm()
+    diabetesform = DiabetesForm()
     if diabetesform.validate_on_submit():
-        username=diabetesform.username.data
+        username = diabetesform.username.data
         global name
         name.append(username)
-        gender=diabetesform.gender.data
-        age=diabetesform.age.data
-        address=diabetesform.address.data
+        gender = diabetesform.gender.data
+        age = diabetesform.age.data
+        address = diabetesform.address.data
         global pincode
         global email
         global phone
@@ -155,176 +155,191 @@ def diabetes():
             flash("Username must be greater than 4 characters.", category='error')
         elif (age) >= 150:
             flash("Age value exceeded", category='error')
-        elif (height)>3:
+        elif (height) > 3:
             flash("Invalid Height Input.", category='error')
         elif (blood_glucose) > 700:
             flash("Invalid blood glucose level", category='error')
         else:
-            flash("Form Submited!", category='success')
-            if hypertension=="Yes":
-                hypertension_int=1
+            flash("Form Submitted!", category='success')
+            if hypertension == "Yes":
+                hypertension_int = 1
             else:
-                hypertension_int=0
-            
-            if previousHeartDisease=="Yes":
-                heart_disease=1
+                hypertension_int = 0
+
+            if previousHeartDisease == "Yes":
+                heart_disease = 1
             else:
-                heart_disease=0
+                heart_disease = 0
 
-            if gender=="Male":
-                gender_int=1
+            if gender == "Male":
+                gender_int = 1
             else:
-                gender_int=0
+                gender_int = 0
 
-            if smoking_History=="Other":
-                smk_curr=0
-                smk_ever=0
-                smk_former=0
-                smk_never=0
-                smk_nt_curr=0
+            if smoking_History == "Other":
+                smk_curr = 0
+                smk_ever = 0
+                smk_former = 0
+                smk_never = 0
+                smk_nt_curr = 0
 
-            elif smoking_History=="Current":
-                smk_curr=1
-                smk_ever=0
-                smk_former=0
-                smk_never=0
-                smk_nt_curr=0
+            elif smoking_History == "Current":
+                smk_curr = 1
+                smk_ever = 0
+                smk_former = 0
+                smk_never = 0
+                smk_nt_curr = 0
 
-            elif smoking_History=="Ever":
-                smk_curr=0
-                smk_ever=1
-                smk_former=0
-                smk_never=0
-                smk_nt_curr=0
-            
-            elif smoking_History=="Former":
-                smk_curr=0
-                smk_ever=0
-                smk_former=1
-                smk_never=0
-                smk_nt_curr=0
+            elif smoking_History == "Ever":
+                smk_curr = 0
+                smk_ever = 1
+                smk_former = 0
+                smk_never = 0
+                smk_nt_curr = 0
 
-            elif smoking_History=="Never":
-                smk_curr=0
-                smk_ever=0
-                smk_former=0
-                smk_never=1
-                smk_nt_curr=0
+            elif smoking_History == "Former":
+                smk_curr = 0
+                smk_ever = 0
+                smk_former = 1
+                smk_never = 0
+                smk_nt_curr = 0
 
-            else:
-                smk_curr=0
-                smk_ever=0
-                smk_former=0
-                smk_never=0
-                smk_nt_curr=1
-
-            bmi=weight/(height*height)
-            if bmi>34:
-                wgt_over=1
-                wgt_under=0
-            
-            elif bmi<14:
-                wgt_over=0
-                wgt_under=1
+            elif smoking_History == "Never":
+                smk_curr = 0
+                smk_ever = 0
+                smk_former = 0
+                smk_never = 1
+                smk_nt_curr = 0
 
             else:
-                wgt_over=0
-                wgt_under=0
+                smk_curr = 0
+                smk_ever = 0
+                smk_former = 0
+                smk_never = 0
+                smk_nt_curr = 1
 
-            if hba1clvl=="Below 3":
-                hba1c_4=0
-                hba1c_5=0
-                hba1c_6=0
-                hba1c_7=0
-                hba1c_8=0
-                hba1c_9=0
+            bmi = weight / (height * height)
+            if bmi > 34:
+                wgt_over = 1
+                wgt_under = 0
 
-            elif hba1clvl=="3-4":
-                hba1c_4=1
-                hba1c_5=0
-                hba1c_6=0
-                hba1c_7=0
-                hba1c_8=0
-                hba1c_9=0
-
-            elif hba1clvl=="4-5":
-                hba1c_4=0
-                hba1c_5=1
-                hba1c_6=0
-                hba1c_7=0
-                hba1c_8=0
-                hba1c_9=0
-
-            elif hba1clvl=="5-6":
-                hba1c_4=0
-                hba1c_5=0
-                hba1c_6=1
-                hba1c_7=0
-                hba1c_8=0
-                hba1c_9=0
-
-            elif hba1clvl=="6-7":
-                hba1c_4=0
-                hba1c_5=0
-                hba1c_6=0
-                hba1c_7=1
-                hba1c_8=0
-                hba1c_9=0
-
-            elif hba1clvl=="7-8":
-                hba1c_4=0
-                hba1c_5=0
-                hba1c_6=0
-                hba1c_7=0
-                hba1c_8=1
-                hba1c_9=0
+            elif bmi < 14:
+                wgt_over = 0
+                wgt_under = 1
 
             else:
-                hba1c_4=0
-                hba1c_5=0
-                hba1c_6=0
-                hba1c_7=0
-                hba1c_8=0
-                hba1c_9=1
+                wgt_over = 0
+                wgt_under = 0
 
-            query = np.array([age,hypertension_int,heart_disease,blood_glucose,gender_int,smk_curr,smk_ever,smk_former,smk_never,smk_nt_curr,wgt_over,wgt_under,hba1c_4,hba1c_5,hba1c_6,hba1c_7,hba1c_8,hba1c_9])
+            if hba1clvl == "Below 3":
+                hba1c_4 = 0
+                hba1c_5 = 0
+                hba1c_6 = 0
+                hba1c_7 = 0
+                hba1c_8 = 0
+                hba1c_9 = 0
 
-            query = query.reshape(1,18)
-            input_trf=scaler.transform(query)
-            prediction=model.predict(input_trf)
+            elif hba1clvl == "3-4":
+                hba1c_4 = 1
+                hba1c_5 = 0
+                hba1c_6 = 0
+                hba1c_7 = 0
+                hba1c_8 = 0
+                hba1c_9 = 0
+
+            elif hba1clvl == "4-5":
+                hba1c_4 = 0
+                hba1c_5 = 1
+                hba1c_6 = 0
+                hba1c_7 = 0
+                hba1c_8 = 0
+                hba1c_9 = 0
+
+            elif hba1clvl == "5-6":
+                hba1c_4 = 0
+                hba1c_5 = 0
+                hba1c_6 = 1
+                hba1c_7 = 0
+                hba1c_8 = 0
+                hba1c_9 = 0
+
+            elif hba1clvl == "6-7":
+                hba1c_4 = 0
+                hba1c_5 = 0
+                hba1c_6 = 0
+                hba1c_7 = 1
+                hba1c_8 = 0
+                hba1c_9 = 0
+
+            elif hba1clvl == "7-8":
+                hba1c_4 = 0
+                hba1c_5 = 0
+                hba1c_6 = 0
+                hba1c_7 = 0
+                hba1c_8 = 1
+                hba1c_9 = 0
+
+            else:
+                hba1c_4 = 0
+                hba1c_5 = 0
+                hba1c_6 = 0
+                hba1c_7 = 0
+                hba1c_8 = 0
+                hba1c_9 = 1
+
+            query = np.array([age, hypertension_int, heart_disease, blood_glucose, gender_int, smk_curr, smk_ever, smk_former, smk_never, smk_nt_curr, wgt_over, wgt_under, hba1c_4, hba1c_5, hba1c_6, hba1c_7, hba1c_8, hba1c_9])
+            query = query.reshape(1, 18)
+            input_trf = scaler.transform(query)
+            prediction = model.predict(input_trf)
             global pred
-            pred=prediction
+            pred = prediction
 
             print(pred)
 
-            
             with app.app_context():
-                checkup_data=Checkup(age=age,
-                                    name=username,
-                                    gender=gender,
-                                    hypertension=hypertension,
-                                    heart_disease=previousHeartDisease,
-                                    smoking_history=smoking_History,
-                                    blood_glucose=blood_glucose,
-                                    weight=weight,
-                                    height=height,
-                                    hba1c=hba1clvl,
-                                    diabetes=pred,
-                                    patient_id=get_current_user().id)
+                checkup_data = Checkup(age=age,
+                                       name=username,
+                                       gender=gender,
+                                       hypertension=hypertension,
+                                       heart_disease=previousHeartDisease,
+                                       smoking_history=smoking_History,
+                                       blood_glucose=blood_glucose,
+                                       weight=weight,
+                                       height=height,
+                                       hba1c=hba1clvl,
+                                       diabetes=pred,
+                                       patient_id=get_current_user().id)
                 db.session.add(checkup_data)
                 db.session.commit()
-                
-            
-            if prediction[0]==0:
-                flash(f"Your chances of diabetes is low !!!", category="success")
-            else:
-                flash(f"Your cances of diabetes is high !!!", category="error")
 
-            send_email(email,"Medassis Report", f"Name : {username}\n Gender : {gender}\n Age : {age}\n Hypertension : {hypertension}\n Previous Heart Disease : {previousHeartDisease}\n Weight : {weight}\n Height : {height}\n HBA1C Level : {hba1clvl}\n Blood Glucose : {blood_glucose}\n Diabetes : {pred}\n")   
+            # Format the email content
+            if prediction[0] == 0:
+                result_message = "Your chances of diabetes are low."
+            else:
+                result_message = "Your chances of diabetes are high."
+
+            email_message = f"""
+            <html>
+            <body>
+                <h2>Diabetes Risk Assessment Report</h2>
+                <p><strong>Name:</strong> {username}</p>
+                <p><strong>Gender:</strong> {gender}</p>
+                <p><strong>Age:</strong> {age}</p>
+                <p><strong>Hypertension:</strong> {hypertension}</p>
+                <p><strong>Previous Heart Disease:</strong> {previousHeartDisease}</p>
+                <p><strong>Weight:</strong> {weight} kg</p>
+                <p><strong>Height:</strong> {height} m</p>
+                <p><strong>HBA1C Level:</strong> {hba1clvl}</p>
+                <p><strong>Blood Glucose:</strong> {blood_glucose} mg/dL</p>
+                <p><strong>Diabetes Risk:</strong> {result_message}</p>
+            </body>
+            </html>
+            """
+
+            send_email(email, "Medassis Report", email_message)
             return redirect(url_for('result_diabetes'))
-            
-        
-    return render_template('diabetesform.html',form=diabetesform)
+
+
 
 @app.route('/result')
 @login_required_user
